495b6b861605ee164f004fa2a1526e55b630a58a
mathiasroemer <mathiasroemer1206@gmail.com>
2024-02-05 11:11:29 +0100

    =vec

---
+++ exercises/week2/vec/vector.cs
@@ -0,0 +1,9 @@
+using System;
+
+class vector{
+	public class vec(){
+		public double x,y,z;
+		public vec(){x,y,z=0;}
+		public vec(double x, double y, double z){this.x=x; this.y=y; this.z=z;}
+	}
+}
